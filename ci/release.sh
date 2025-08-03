837ae04ff214326e87073b5b95021e2c1d0c70b1
Larissa Rosalene <lerarosalene@outlook.com>
2025-08-03 20:35:28 +0300

    remove redundant check

--- ci/release.sh
+++ ci/release.sh
@@ -7,11 +7,6 @@ TAG="v${VERSION}"
 NAME="$(cat package.json | jq .name -r)"
 UPDATE_URL="https://github.com/${REPOSITORY}/releases/latest/download/${NAME}.user.js"
 
-if [ $(git tag -l "${TAG}") ]; then
-  echo "Can't create release for tag ${TAG}: ${TAG} already exists"
-  exit 1
-fi
-
 npm ci
 npm run typecheck
 UPDATE_URL="${UPDATE_URL}" npm run build
